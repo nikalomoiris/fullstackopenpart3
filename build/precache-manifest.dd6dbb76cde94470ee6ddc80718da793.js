94b535defac02498f6fbb1790a42b9523a142412
nikalomiris <nikalomiris@gmail.com>
2019-07-09 13:02:27 -0400

    integrated frontend react app

---
+++ build/precache-manifest.dd6dbb76cde94470ee6ddc80718da793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c087adf1eb6974892d26cd805fcf116c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ac29791701e0a7dcbb",
+    "url": "/static/css/main.2eef2b4a.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "69ac29791701e0a7dcbb",
+    "url": "/static/js/main.1a99ca85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
