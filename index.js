80e7dd7cff060361e3384bf979bcc21e908fea15
nikalomiris <nikalomiris@gmail.com>
2019-07-10 15:51:35 -0400

    Added ESlint.

--- index.js
+++ index.js
@@ -13,44 +13,44 @@ app.use(morgan('tiny'));
 
 let persons = [
     {
-        "name": "Ada Lovelace",
-        "phone": "39-44-5323523",
-        "id": 2
+        'name': 'Ada Lovelace',
+        'phone': '39-44-5323523',
+        'id': 2
     },
     {
-        "name": "Dan Abramov",
-        "phone": "12-43-234345",
-        "id": 3
+        'name': 'Dan Abramov',
+        'phone': '12-43-234345',
+        'id': 3
     },
     {
-        "name": "Mary Poppendieck",
-        "phone": "39-23-6423122",
-        "id": 4
+        'name': 'Mary Poppendieck',
+        'phone': '39-23-6423122',
+        'id': 4
     },
     {
-        "name": "Nikos Kalomiris",
-        "phone": "39-23-6423123",
-        "id": 5
+        'name': 'Nikos Kalomiris',
+        'phone': '39-23-6423123',
+        'id': 5
     },
     {
-        "name": "dfdok",
-        "phone": "23244",
-        "id": 9
+        'name': 'dfdok',
+        'phone': '23244',
+        'id': 9
     },
     {
-        "name": "a",
-        "phone": "4444",
-        "id": 10
+        'name': 'a',
+        'phone': '4444',
+        'id': 10
     },
     {
-        "name": "b",
-        "phone": "222",
-        "id": 11
+        'name': 'b',
+        'phone': '222',
+        'id': 11
     },
     {
-        "name": "c",
-        "phone": "333",
-        "id": 12
+        'name': 'c',
+        'phone': '333',
+        'id': 12
     }
 ];
 
@@ -103,16 +103,16 @@ app.post('/api/persons', (req, res, next) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    console.error(error.message);
 
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
-      return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' });
     }
 
-    next(error)
-}
+    next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
