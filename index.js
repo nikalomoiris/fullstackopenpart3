cf2cdf20a91a3eeb233cffc80cb7f2c60138e912
nikalomiris <nikalomiris@gmail.com>
2019-07-10 14:02:26 -0400

    Implemented error handling

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/person');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(morgan('tiny'));
 app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
 let persons = [
     {
@@ -64,10 +64,16 @@ app.get('/info', (req, res) => {
     res.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p></div>`)
 });
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person.toJSON());
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person.toJSON());
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -96,6 +102,18 @@ app.post('/api/persons', (req, res) => {
         res.json(savedPerson.toJSON());
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
